9f051a1a505491931c635dbd5c1dbbede1419c6c
Max Denisenko <meetmorrowsolonmars@gmail.com>
2023-10-28 02:37:13 +0300

    test kafka consumer and added service kafka to docker compose

--- answer/cmd/answer/main.go
+++ answer/cmd/answer/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"log"
 
+	"github.com/IBM/sarama"
 	"github.com/google/uuid"
 	"google.golang.org/protobuf/encoding/protojson"
 
@@ -34,4 +36,52 @@ func main() {
 	}
 
 	log.Println(string(payload))
+
+	brokers := []string{"kafka:29092"}
+	groupID := "answer"
+	consumerGroupConfig := sarama.NewConfig()
+	consumerGroupConfig.Consumer.Group.Rebalance.GroupStrategies = []sarama.BalanceStrategy{
+		sarama.NewBalanceStrategyRoundRobin(),
+		sarama.NewBalanceStrategyRange(),
+	}
+	// TODO: test it
+	consumerGroupConfig.Consumer.Offsets.Initial = sarama.OffsetNewest
+
+	consumerGroup, err := sarama.NewConsumerGroup(brokers, groupID, consumerGroupConfig)
+	if err != nil {
+		log.Fatalf("can't create consumer group: %s", err)
+	}
+
+	ctx := context.Background()
+	err = consumerGroup.Consume(ctx, []string{"answers_test"}, &handler{})
+	if err != nil {
+		log.Fatalf("can't start consumer group: %s", err)
+	}
+}
+
+type handler struct {
+}
+
+func (h handler) Setup(_ sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (h handler) Cleanup(_ sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (h handler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for {
+		select {
+		case msg, ok := <-claim.Messages():
+			if !ok {
+				log.Printf("msg channel was closed")
+				return nil
+			}
+			log.Printf("Message claimed: value = %s, timestamp = %v, topic = %s", string(msg.Value), msg.Timestamp, msg.Topic)
+			session.MarkMessage(msg, "")
+		case <-session.Context().Done():
+			return nil
+		}
+	}
 }
--- profile/go.mod
+++ profile/go.mod
@@ -19,10 +19,10 @@ require (
 	github.com/prometheus/client_model v0.4.1-0.20230718164431-9a2bf3000d16 // indirect
 	github.com/prometheus/common v0.44.0 // indirect
 	github.com/prometheus/procfs v0.11.1 // indirect
-	github.com/stretchr/testify v1.8.3 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
 )
